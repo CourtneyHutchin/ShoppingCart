7174a06d950ce6cb9738ff7a015dfd7f185af119
CourtneyHutchin <courtneyhutchin47@gmail.com>
2019-11-12 13:46:48 -0800

    Created a Product class

---
+++ typescripts/product.ts
@@ -0,0 +1,21 @@
+/**
+ * Represents a single shopping cart item
+ */
+
+class Product {
+    /**
+     * Name of the product 
+     */
+    title: string;
+    description: string;
+    price: Number;
+}
+
+
+// Test Code
+/*
+let product = new Product
+product.title = "something";
+product.description = "something something";
+product.price = 4.99;
+*/
